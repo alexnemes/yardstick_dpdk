25c4a2c195961337f1558aa12201e6a55ef8a266
Alexandru Nemes <alexandru.nemes@enea.com>
2017-08-14 18:40:51 +0300

    removed extra spaces

--- tests/ci/load_images.sh
+++ tests/ci/load_images.sh
@@ -48,7 +48,7 @@ build_yardstick_image()
         if [ ! -f "${RAW_IMAGE}" ];then
             local cmd
             cmd="sudo $(which yardstick-img-lxd-modify) $(pwd)/tools/ubuntu-server-cloudimg-modify.sh"
-            cmd2 = "sudo $(which bash yardstick-img-dpdk-finalize.sh)"
+            cmd2="sudo $(which bash yardstick-img-dpdk-finalize.sh)"
             
             # Build the image. Retry once if the build fails
             $cmd || $cmd
