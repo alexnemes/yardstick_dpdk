54204965b5f9dd97b54baefb9dd89902cf4c458d
Alexandru Nemes <alexandru.nemes@enea.com>
2017-08-17 15:28:18 +0300

    update bind script path; add flavor param

--- tests/ci/load_images.sh
+++ tests/ci/load_images.sh
@@ -235,7 +235,8 @@ create_nova_flavor()
         # delete flavor, if it exists, befoare creating, in order to avoid conflict
         openstack ${SECURE} flavor delete yardstick-dpdk-flavor &> /dev/null || true
         # Create the nova flavor used by some sample test cases
-        openstack ${SECURE} flavor create --id 100 --ram 4096 --disk 4 --vcpus 4 yardstick-dpdk-flavor
+        openstack ${SECURE} flavor create --id 100 --ram 4096 --disk 4 --vcpus 4 yardstick-dpdk-flavor 
+        #nova flavor-key yardstick-dpdk-flavor set hw:mem_page_size=large
         # DPDK-enabled OVS requires guest memory to be backed by large pages
         if [[ $DEPLOY_SCENARIO == *[_-]ovs[_-]* ]]; then
             openstack ${SECURE} flavor set --property hw:mem_page_size=large yardstick-flavor
