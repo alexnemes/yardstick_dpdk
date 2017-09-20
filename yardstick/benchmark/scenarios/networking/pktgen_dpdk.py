0f21431aedc377ae2882083cf0716e72a0fab819
Alexandru Nemes <alexandru.nemes@enea.com>
2017-09-20 18:08:43 +0300

    check optimized port security. TOL 0.1

--- yardstick/benchmark/scenarios/networking/pktgen_dpdk.py
+++ yardstick/benchmark/scenarios/networking/pktgen_dpdk.py
@@ -90,6 +90,7 @@ class PktgenDPDKTputLatency(base.Scenario):
                 q = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=d)
                 for line in q.stdout.readlines():
                     print("line: {}".format(line))
+                    print line.split("|")[2].strip()
                     if line.split("|")[2].strip() != False:
                 
                         
